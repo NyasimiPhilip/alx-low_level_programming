a4c8322987343dd2aebbe426a77d1918952674e5
enwai <nyasimiphilip@gmail.com>
2023-02-22 00:34:49 +0300

    ALX

--- 0x02-functions_nested_loops/4-main.c
+++ 0x02-functions_nested_loops/4-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - check the code.
  *
@@ -7,12 +8,15 @@
 int main(void)
 {
 	int r;
-	r = _islower('H');
+
+	r = _isalpha('H');
 	_putchar(r + '0');
-	r = _islower('o');
+	r = _isalpha('o');
 	_putchar(r + '0');
-	r = _islower(108);
+	r = _isalpha(108);
 	_putchar(r + '0');
-       	_putchar('\n');
+	r = _isalpha(';');
+	 _putchar(r + '0');
+	_putchar('\n');
 	return (0);
 }
