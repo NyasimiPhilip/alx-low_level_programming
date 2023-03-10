21ba8a31c6baeae718c267d9ae961ba19e2e4106
enwai <nyasimiphilip@gmail.com>
2023-03-10 13:15:43 +0300

    ALX

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
-	/**
-	 * main - Entry point
-	 *
-	 * Return: Always 0 (Success)
-	 */
-int main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void _puts(char*str)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	while(*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
 }
