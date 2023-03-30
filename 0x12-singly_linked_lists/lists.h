7af6feb859aa2d9b94d65287e219999deaa65fff
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-03-30 15:40:31 +0300

    ALX

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,22 +1,28 @@
-#ifndef LISTS_H
-#define LISTS_H
-/**
- * struct listint_s - singly linked list
- * @n: integer
+#ifndef _LISTS_
+#define _LISTS_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/*
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
+ * for Holberton project
  */
-typedef struct listint_s
+typedef struct list_s
 {
-		char *str;
-		struct listint_s *next;
-		unsigned int len;
-} listint_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-size_t print_listint(const listint_t *h);
-size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
-listint_t *add_nodeint_end(listint_t **head, const int n);
-void free_listint(listint_t *head);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
-#endif /* LISTS_H */
+#endif
