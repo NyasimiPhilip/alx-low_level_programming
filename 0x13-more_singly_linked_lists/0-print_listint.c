6cd12e011e5429ee859111b48e40870892aafce5
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-04-03 20:10:45 +0300

    ALX

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-		while (h)
-		{
-			nodes++;
-			printf("%d\n", h->n);
-			h = h->next;
-		}
-		return (nodes);
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	
+	return (nodes);
 }
