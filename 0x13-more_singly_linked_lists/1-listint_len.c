1a9789801f8cb8333b1e7873fbfe3544dad84170
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-04-03 19:06:03 +0300

    ALX

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *head)
+{
+	size_t count = 0;
+
+	const listint_t *current_node = head;
+	while (current_node != NULL) {
+		count++;
+		current_node = current_node->next;
+	}
+	return count;
+}
