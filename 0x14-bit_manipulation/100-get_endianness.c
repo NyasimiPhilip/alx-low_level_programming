4ad0093376d43837fd70109b78a6b65a01dd798c
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-04-06 19:35:39 +0300

    ALX

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,12 +1,18 @@
+/*
+ * File: 100-get_endianness.c
+ * Author Nyasimi Philip
+ */
 #include "main.h"
-/**
+/*
  * get_endianness - checks if a machine is little or big endian
  * Return: 0 for big, 1 for little
  */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *) &i;
+	int num = 1;
+	char *endian = (char *)&num;
 
-	return (*c);
+	if (*endian == 1)
+		return (1);
+	return (0);
 }
