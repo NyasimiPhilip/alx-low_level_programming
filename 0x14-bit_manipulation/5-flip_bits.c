8727db730803bd112d53395196def68cbf12f2ab
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-04-06 18:48:42 +0300

    ALX

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned long int diff = n ^ m;
+
+	while (diff != 0)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
+	return count;
+}
