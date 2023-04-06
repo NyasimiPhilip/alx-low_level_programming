e584031cb475415db3b2c3dbb50fd26d18015104
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-04-06 17:49:01 +0300

    ALX

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,26 +2,21 @@
 /**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary(1 or 0).
- */
+*/
 void print_binary(unsigned long int n)
 {
-	int i;
-	unsigned long int mask = 1UL << 63;
+	unsigned int mask = 1 << 31;
 
-	for (i = 0; i < 64; i++)
+	while (mask > 0)
 	{
 		if (n & mask)
 		{
 			_putchar('1');
 		}
-		else if (i != 0)
+		else
 		{
 			_putchar('0');
 		}
-		n <<= 1;
-	}
-	if (i == 64)
-	{
-		_putchar('0');
+		mask >>= 1;
 	}
 }
