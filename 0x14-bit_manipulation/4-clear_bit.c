680a05ba9155092b82d988d8ff905d3c37bd6c70
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-04-29 13:33:25 +0300

    ALX

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,9 @@
 /**
- *  File: 4-clear_bit.c
- */
+ * clear_bit - Clears the bit at given index in unsigned integer
+ * @n:a pointer to an unsigned 64-bit integer
+ * @index: is an unsigned integer
+ * Return: 1 or -1
+ **/
 #include "main.h"
 
 int clear_bit(unsigned long int *n, unsigned int index)
