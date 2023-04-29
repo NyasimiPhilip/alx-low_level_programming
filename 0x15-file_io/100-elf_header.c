b0c29fea1e05f4bc4dc5fd30505844b4f973a5a9
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-04-29 20:11:17 +0300

    Update 100-elf_header.c
    ALX

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
 #include <sys/stat.h>
 #include <string.h>
-#include <elf.h>
-
-void print_elf_header_info(Elf64_Ehdr* header);
+#include <errno.h>
+#ifdef __NetBSD__
+#include <sys/exec_elf.h>
+#endif
 
 int main(int argc, char** argv) {
     const char* filename;
@@ -54,27 +56,36 @@ int main(int argc, char** argv) {
     printf("%s ELF %s %s\n", filename, elf_data,
            header.e_ident[EI_DATA] == ELFDATA2MSB ? "big endian" : "little endian");
 
-    print_elf_header_info(&header);
+    printf("entry point: %lx\n", header.e_entry);
+
+    printf("start of section headers: %ld (bytes into file)\n", header.e_shoff);
+
+    printf("number of section headers: %d\n", header.e_shnum);
+
+    printf("size of section headers: %d (bytes)\n", header.e_shentsize);
+
+    printf("start of program headers: %ld (bytes into file)\n", header.e_phoff);
+
+    printf("number of program headers: %d\n", header.e_phnum);
+
+    printf("size of program headers: %d (bytes)\n", header.e_phentsize);
+
+#ifdef __NetBSD__
+    switch (header.e_machine) {
+        case EM_NETBSD:
+            printf("This ELF file is compiled for NetBSD\n");
+            break;
+        default:
+            printf("This ELF file is not compiled for NetBSD\n");
+            break;
+    }
+#else
+    printf("This ELF file is not compiled for NetBSD\n");
+#endif
 
     rc = 0;
 
 out:
     close(fd);
     return rc;
-}
-
-void print_elf_header_info(Elf64_Ehdr* header) {
-    printf("entry point: %lx\n", header->e_entry);
-
-    printf("start of section headers: %ld (bytes into file)\n", header->e_shoff);
-
-    printf("number of section headers: %d\n", header->e_shnum);
-
-    printf("size of section headers: %d (bytes)\n", header->e_shentsize);
-
-    printf("start of program headers: %ld (bytes into file)\n", header->e_phoff);
-
-    printf("number of program headers: %d\n", header->e_phnum);
-
-    printf("size of program headers: %d (bytes)\n", header->e_phentsize);
 }
