8c64ca7a23b10763ddd77614a808279bf75ab40e
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-05-04 18:36:15 +0300

    ALX

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert a binary string to an unsigned int
+ * @b: pointer to a string of 0 and 1 characters
+ * This function converts a binary string to an unsigned int.
+ * Return:int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len;
+	unsigned int num = 0, mult = 1;
+
+	if (!b)
+	{
+		return (0);
+	}
+	for (len = 0; b[len] != '\0'; len++)
+	{
+		;
+	}
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+		num += (b[len] - '0') * mult;
+		mult *= 2;
+	}
+	return (num);
+}
