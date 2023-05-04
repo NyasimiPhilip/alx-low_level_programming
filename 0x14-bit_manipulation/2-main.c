54a8730ced0c486c2b59f07963954582efd71c9d
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-05-04 21:26:43 +0300

    ALX

---
+++ 0x14-bit_manipulation/2-main.c
@@ -0,0 +1,18 @@
+/**
+ *  get_bit - returns the value of the bit
+ *  @n: is an unsigned long integer
+ *  @index: is an unsigned integer
+ *  Return: int
+ */
+#include "main.h"
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	mask <<= index;
+	if (n & mask)
+		return (1);
+	return (0);
+}
