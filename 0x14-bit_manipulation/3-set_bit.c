e9a45f0511432e365bc63c040082b9a799ef3cb3
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-05-04 21:56:54 +0300

    ALX

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+/**
+ * set_bit - Sets the bit at the given index
+ * @n: is a pointer to an unsigned 64-bit integer
+ * @index: is an unsigned integer
+ * Return: int
+ */
+#include "main.h"
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n ^= (1 << index);
+	return (1);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+/**
+ * clear_bit - Clears the bit at given index in unsigned integer
+ * @n:a pointer to an unsigned 64-bit integer
+ * @index: is an unsigned integer
+ * Return: 1 or -1
+ */
+#include "main.h"
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	mask <<= index;
+	*n &= ~mask;
+	return (1);
+}
