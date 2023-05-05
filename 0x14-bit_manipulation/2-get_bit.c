99f2c4f94a272180b3c660fd7c17a8066bfc867d
NyasimiPhilip <nyasimiphilip@gmail.com>
2023-05-05 11:52:50 +0300

    ALX

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_bit - The function gets the value of a bit
+ * @n: number containing the bit
+ * @index: position of the bit
+ * Return: int
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+	return ((n >> index) & 1);
+}
