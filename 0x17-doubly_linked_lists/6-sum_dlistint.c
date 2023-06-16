c325803e217888127eef5c95e6ab7a9afd587970
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-16 05:40:53 +0300

    Create 6-sum_dlistint.c

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Calculates the sum of data in a doubly linked list.
+ * @h: Pointer to the head of the list.
+ * Return: The sum of the data.
+ */
+int sum_dlistint(dlistint_t *h)
+{
+	int totalSum = 0;
+	dlistint_t *currentNode = h;
+
+	while (currentNode)
+	{
+		totalSum += currentNode->n;
+		currentNode = currentNode->next;
+	}
+
+	return totalSum;
+}
