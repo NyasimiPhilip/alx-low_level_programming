a627dde2a524e69b955673e2baf9605f5d25553e
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-06-16 16:45:29 +0300

    Update

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,6 @@
 /*
  * File: 7-insert_dnodeint.c
  */
-
 #include "lists.h"
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int num)
@@ -10,32 +9,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int index, int 
 
 	if (index == 0)
 		return (add_dnodeint(head, num));
-
 	for (; index != 1; index--)
 	{
 		temp = temp->next;
 		if (temp == NULL)
 			return (NULL);
 	}
-
 	if (temp->next == NULL)
 		return (add_dnodeint_end(head, num));
-
 	newNode = malloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
-
 	newNode->n = num;
 	newNode->prev = temp;
 	newNode->next = temp->next;
 	temp->next->prev = newNode;
 	temp->next = newNode;
-
 	return (newNode);
 }
-
-
-
-
-
-
