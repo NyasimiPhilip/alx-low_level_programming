6c9fd9193dad668c40e394251fd30194aa870241
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-06-16 05:17:03 +0300

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_len - get number of nodes in a list
+ * @h: head ptr
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+  size_t node_count = 0;
+  const dlistint_t *traverse = h;
+  
+  while (traverse != NULL)
+  {
+    node_count++;
+    traverse = traverse->next;
+  }
+  return (node_count);
+}
