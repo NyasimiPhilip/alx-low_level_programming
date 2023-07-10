ae0124492516c3a84cb3cc5e07c980aeed9e7ab4
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-07-10 12:15:24 +0300

    Update

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * print_hash_table - Prints the contents of the hash table.
+ * @ht: A pointer to the hash table.
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+	char *separator;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	separator = "";
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			printf("%s'%s': '%s'", separator, current->key, current->value);
+			separator = ", ";
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
