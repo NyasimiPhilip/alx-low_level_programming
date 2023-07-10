d18dedb4d44d2af4ef33d01c22b23dd5a324ccc4
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-07-10 10:36:53 +0300

    Update

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+/**
+ * hash_djb2 - Generates a unique hash value using a custom algorithm.
+ * @str: The string used to generate the hash value.
+ *
+ * Return: The unique hash value.
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
