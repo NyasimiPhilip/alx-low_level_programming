b8c117cca7e687f374b8dc823c707a9f2e58861d
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-10-01 23:01:43 +0300

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all single digit numbers of base
- * 10 starting from 0, followed by a new line
+ * 10 starting from 0, followed by a new line.
  * Return: Always 0 (Success)
  */
 int main(void)
