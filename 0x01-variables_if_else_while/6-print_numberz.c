c8c3484f08a82667d1699e361dc3e06ae80859d2
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-10-01 23:02:09 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line, using putchar
+ * followed by a new line, using putchar.
  * Return: Always 0 (Success)
  */
 int main(void)
