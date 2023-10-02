ed6ad1fd1aaeed3ffaf2e57cae2260a11a11b0f7
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-10-02 23:04:47 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * @n: the int to check
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
+ * -1 and prints - if n is less than zero.
  */
 int print_sign(int n)
 {
