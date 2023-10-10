e75664a84b76ed86fe374d21969bd90cd1487aee
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-10-10 18:10:37 +0300

    Update

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+/**
+ * jump_search - jump search algo
+ * @array: array passed
+ * @size: size of array
+ * @value: value to be searched
+ * Return: index of value if found, else -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int jump_steps, left, right;
+	int array_size = size;
+
+	jump_steps = sqrt(array_size);
+	left = 0, right = jump_steps;
+	while (left < array_size && array[left] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", left, array[left]);
+		left += jump_steps;
+	}
+	printf("Value found between indexes [%d] and [%d]\n",
+			left - jump_steps, left);
+	right = left;
+	left -= jump_steps;
+	for (; left <= right && left < array_size; left++)
+	{
+		printf("Value checked array[%d] = [%d]\n", left, array[left]);
+		if (array[left] == value)
+			return (left);
+	}
+	return (-1);
+}
