cd02bd40b14d1c3c9ea96f679f375569c96d6f0d
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-10-24 19:19:07 +0300

    Update 1-main.c

--- 0x07-pointers_arrays_strings/1-main.c
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -5,7 +5,6 @@
  * simple_print_buffer - prints buffer in hexa
  * @buffer: the address of memory to print
  * @size: the size of the memory to print
- *
  * Return: Nothing.
  */
 void simple_print_buffer(char *buffer, unsigned int size)
