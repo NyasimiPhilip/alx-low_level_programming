31f79ff57d404474329fa163e275ec1c101ed6f5
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-04 17:52:58 +0300

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,7 +4,6 @@
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
- *
  * Return: Always 0 (Success)
  */
 int main(int argc __attribute__((unused)), char *argv[])
