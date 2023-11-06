7861df0e7e3dd9990cd9f2ea34b9eecd38d94f13
NyasimiPhilip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-06 13:39:57 +0300

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,6 @@ char *_memset(char *s, char b, unsigned int n)
  * _calloc - allocates memory for an array
  * @nmemb: number of elements in the array
  * @size: size of each element
- *
  * Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
