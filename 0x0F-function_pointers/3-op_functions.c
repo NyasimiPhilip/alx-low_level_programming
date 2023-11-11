bab3b8c9cf198f1db60b045cb03197427b01b32a
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-11 16:10:31 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,7 +8,7 @@ int op_mod(int a, int b);
  *  op_add - Returns the sum of two numbers.
  *  @a: The first number.
  *  @b: The second number.
- *  Return: The sum of a and b.
+ *  Return: The sum of a and b
  */
 int op_add(int a, int b)
 {
