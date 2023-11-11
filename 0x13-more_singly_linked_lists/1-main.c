c7ff8fd99f706b891ebb9a952dbe948a626e3a42
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-11-11 16:55:08 +0300

    Update

--- 0x13-more_singly_linked_lists/1-main.c
+++ 0x13-more_singly_linked_lists/1-main.c
@@ -16,14 +16,15 @@ int main(void)
 	head = &hello;
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
-	{				printf("Error\n");
-															return (1);
-																}
-									new->n = 9;
-										new->next = head;
-											head = new;
-												n = listint_len(head);
-													printf("-> %lu elements\n", n);
-														free(new);
-															return (0);
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->n = 9;
+	new->next = head;
+	head = new;
+	n = listint_len(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return (0);
 }
