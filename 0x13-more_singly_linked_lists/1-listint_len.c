1ba4769495263a23a004ee20ec9681be1016b960
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-11 16:46:15 +0300

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 
-/**
+/*
  * listint_len - returns the number of elements in a linked lists
  * @h: linked list of type listint_t to traverse
  * Return: number of nodes
