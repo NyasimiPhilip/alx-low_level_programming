992c89b3dea3bcbe04b68222650fc0fd2c7daea1
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-12 08:20:23 +0300

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
+ * reverse_listint - reverses a linked list.
  * @head: pointer to the first node in the list
  * Return: pointer to the first node in the new list
  */
