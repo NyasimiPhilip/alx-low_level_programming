9552ebab03755e3a09659e2fd72ea04686ea6d9f
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-12 08:24:15 +0300

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,10 +4,10 @@
  */
 #include "lists.h"
 /**
- * free_listint2 - Frees a listint_t list.
+ * free_listint2 - Frees a listint_t list
  * @head: A pointer to the address of the
- *  head of the listint_t list.
- * Description: Sets the head to NULL.
+ *  head of the listint_t list
+ * Description: Sets the head to NULL
  */
 void free_listint2(listint_t **head)
 {
