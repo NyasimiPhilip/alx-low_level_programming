96bc8221b01e9c596e659e816f3bf444046e391b
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-12 08:26:09 +0300

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,10 +2,10 @@
 
 /**
  * sum_listint - Calculates the sum of all the
- *  data (n) of a listint_t list.
- * @head: A pointer to the head of the listint_t list.
- * Return: If the list is empty - 0.
- *  Otherwise - the sum of all the data.
+ *  data (n) of a listint_t list
+ * @head: A pointer to the head of the listint_t list
+ * Return: If the list is empty - 0
+ *  Otherwise - the sum of all the data
  */
 int sum_listint(listint_t *head)
 {
