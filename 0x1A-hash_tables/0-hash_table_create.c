b031ff98d9f3d630215d964b2afd29d489319450
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-19 08:26:51 +0300

    Update 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - Creates a hash table instance.
- * @size: The size of the hash table array.
- * Return: If memory allocation fails - NULL.
- * Otherwise - a pointer to the newly created hash table.
+ * hash_table_create - Creates a hash table instance
+ * @size: The size of the hash table array
+ * Return: If memory allocation fails - NULL
+ * Otherwise - a pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
