7090733c67f499ffada5a083540aa26edcc451cb
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-20 09:01:11 +0300

    Update hash_tables.h

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -9,7 +9,7 @@
  * struct shash_node_s - Node of a sorted hash table
  *
  * @key: The key, string
- * The key is unique in the HashTable
+ * The key is unique in the HashTable.
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  * @sprev: A pointer to the previous element of the sorted linked list
