850473fa63844821db57421a46e57d72ab3ee4bf
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-20 08:57:45 +0300

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 /**
  * hash_table_get- Retrieves the value associated with a key
- * @ht: A pointer to the hash table.
- * @key: The key to search for.
- * Return: The value associated with the key, or NULL if not found.
+ * @ht: A pointer to the hash table
+ * @key: The key to search for
+ * Return: The value associated with the key, or NULL if not found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
