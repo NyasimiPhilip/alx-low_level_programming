9f165452dacce6a0021c77bc29e26efe54b278e3
Nyasimi Philip <117896206+NyasimiPhilip@users.noreply.github.com>
2023-11-20 08:47:05 +0300

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 /**
- * key_index - Calculates the index of a key using a custom algorithm.
- * @key: The key.
- * @size: The size of the array.
- * Return: The index of the key.
+ * key_index - Calculates the index of a key using a custom algorithm
+ * @key: The key
+ * @size: The size of the array
+ * Return: The index of the key
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
